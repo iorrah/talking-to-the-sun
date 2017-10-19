a8c81a747893f2695770cd9eca0cb0c08019da14
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-19 18:21:32 +0200

    Implementing initial UI structure

---
+++ src/app/footer/footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>
+          <a href="https://github.com/iorrah/talking-to-the-sun" target="_blank">
+            Fork this project on GitHub
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
